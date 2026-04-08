2c6f0be4b4b1e29fc42d5be9e4fd5d4dc0c43962
Farhanmurtadho7204 <2200005106@webmail.uad.ac.id>
2026-04-08 18:20:15 +0700

    Ujicoba-ketujuh

---
+++ story_content/user.js
@@ -0,0 +1,1253 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5jRvlRGriaT":
+        Script1();
+        break;
+      case "5qT1VGMYIbe":
+        Script2();
+        break;
+      case "6UbPp9WZb21":
+        Script3();
+        break;
+      case "5tEY8nw4jiN":
+        Script4();
+        break;
+      case "60XjKGYvat9":
+        Script5();
+        break;
+      case "5aJPHQEIcyN":
+        Script6();
+        break;
+      case "6Dmx5isWfQR":
+        Script7();
+        break;
+      case "6VyJkP3dzy0":
+        Script8();
+        break;
+      case "6jcBHavPytK":
+        Script9();
+        break;
+      case "5bAjJRVCo59":
+        Script10();
+        break;
+      case "69Ej66nVc1J":
+        Script11();
+        break;
+      case "5ljGLIEKaoM":
+        Script12();
+        break;
+      case "6UoFB2ehNPb":
+        Script13();
+        break;
+      case "5VKk9sJzgTh":
+        Script14();
+        break;
+      case "5nJXiNqCDUD":
+        Script15();
+        break;
+      case "5jW1vgsbEIi":
+        Script16();
+        break;
+      case "5bglckTrEnA":
+        Script17();
+        break;
+      case "5frI4n9aL5h":
+        Script18();
+        break;
+      case "66e1ILzH7z3":
+        Script19();
+        break;
+      case "6YMIcZKxvOb":
+        Script20();
+        break;
+      case "6Elr9Aj73uP":
+        Script21();
+        break;
+      case "6o60HvwNyfE":
+        Script22();
+        break;
+      case "6popad55T4r":
+        Script23();
+        break;
+      case "6fWTF63TSna":
+        Script24();
+        break;
+      case "6E6xD8wCnay":
+        Script25();
+        break;
+      case "5tPtJ5M2xFF":
+        Script26();
+        break;
+      case "6d4wSj9XJXl":
+        Script27();
+        break;
+      case "5VPjvPU0NUT":
+        Script28();
+        break;
+      case "5u3X0w1LLTH":
+        Script29();
+        break;
+      case "6pB72IoICiM":
+        Script30();
+        break;
+      case "5uXja7Z4Lew":
+        Script31();
+        break;
+      case "5l3FHAuX0DK":
+        Script32();
+        break;
+      case "65kj7k0ric2":
+        Script33();
+        break;
+      case "65z0E3s50Gp":
+        Script34();
+        break;
+      case "5jm7YwfOsVe":
+        Script35();
+        break;
+      case "5lw9MYtUC8n":
+        Script36();
+        break;
+      case "6E9Bq7shmoA":
+        Script37();
+        break;
+      case "6Mtf7CcBvfI":
+        Script38();
+        break;
+      case "6N6ltYQRMmJ":
+        Script39();
+        break;
+      case "6TIELqnCWFs":
+        Script40();
+        break;
+      case "5pHr7AShfr9":
+        Script41();
+        break;
+      case "5h8d5FKdNQw":
+        Script42();
+        break;
+      case "5oLAloXcLuQ":
+        Script43();
+        break;
+      case "6NyHe5SMr6a":
+        Script44();
+        break;
+      case "5WFd4Aji53g":
+        Script45();
+        break;
+      case "6FScL3QO8eh":
+        Script46();
+        break;
+      case "5dbwnl6Afka":
+        Script47();
+        break;
+      case "6qFqUWrYkkJ":
+        Script48();
+        break;
+      case "6UnprSS6ohb":
+        Script49();
+        break;
+      case "5uV16LbsvjG":
+        Script50();
+        break;
+      case "5bfEnRnF3Te":
+        Script51();
+        break;
+      case "66HTMyLpXF5":
+        Script52();
+        break;
+      case "5sZvQAEs1EM":
+        Script53();
+        break;
+      case "5kzBlEFSPoG":
+        Script54();
+        break;
+      case "6UCRcqpjYJ1":
+        Script55();
+        break;
+      case "5qU5ZWqQrX6":
+        Script56();
+        break;
+      case "5aYANpIzBd4":
+        Script57();
+        break;
+      case "6kmKQIiRJSy":
+        Script58();
+        break;
+      case "678P7GpJVVE":
+        Script59();
+        break;
+      case "6PqZkL6ha4F":
+        Script60();
+        break;
+      case "5cmyR0Tz1pQ":
+        Script61();
+        break;
+      case "6HnEQoSyARu":
+        Script62();
+        break;
+      case "5zgo3Elg5QY":
+        Script63();
+        break;
+      case "65opQldj6Ef":
+        Script64();
+        break;
+      case "6qMLbn6jHRJ":
+        Script65();
+        break;
+      case "5juSce7crG9":
+        Script66();
+        break;
+      case "6WyVQccHUGX":
+        Script67();
+        break;
+      case "6ecCI6JSixk":
+        Script68();
+        break;
+      case "66tXlsNWdio":
+        Script69();
+        break;
+      case "6QGcI7lrCHn":
+        Script70();
+        break;
+      case "62uOJFhwCiE":
+        Script71();
+        break;
+      case "5VELidSwwcm":
+        Script72();
+        break;
+      case "6ZI7klcEQu9":
+        Script73();
+        break;
+      case "5sRdi18P7tW":
+        Script74();
+        break;
+      case "6gR35vGiWXQ":
+        Script75();
+        break;
+      case "5iNBoC5PfFm":
+        Script76();
+        break;
+      case "5yfxttovZ0r":
+        Script77();
+        break;
+      case "6ZqTNTr37qG":
+        Script78();
+        break;
+      case "6qUfjwonQ6x":
+        Script79();
+        break;
+      case "60KjcyeJp9J":
+        Script80();
+        break;
+      case "6qXX7fm1uuf":
+        Script81();
+        break;
+      case "6iPyq3Zxtjm":
+        Script82();
+        break;
+      case "6mVnfrYfWLX":
+        Script83();
+        break;
+      case "6XHWDj1gJti":
+        Script84();
+        break;
+      case "6gzrE3KHW4O":
+        Script85();
+        break;
+      case "6oe8FwGUa6z":
+        Script86();
+        break;
+      case "6fdSU4NpoHO":
+        Script87();
+        break;
+      case "64FqKKJvgXG":
+        Script88();
+        break;
+      case "5iYPB50KbHx":
+        Script89();
+        break;
+      case "5d7bACrrhrd":
+        Script90();
+        break;
+      case "5oMGfERVyQB":
+        Script91();
+        break;
+      case "5di0r0SiOYt":
+        Script92();
+        break;
+      case "5b1UrN4qLyG":
+        Script93();
+        break;
+      case "6nBzaU7ypyK":
+        Script94();
+        break;
+      case "5g6lU2fhT2h":
+        Script95();
+        break;
+      case "6mHn4P2zXJe":
+        Script96();
+        break;
+      case "5zwPeG5dqB9":
+        Script97();
+        break;
+      case "6UIQhIWs96g":
+        Script98();
+        break;
+      case "6T71MwAQOAM":
+        Script99();
+        break;
+      case "5tglwvzy7rA":
+        Script100();
+        break;
+      case "5ipIRLMI5Hm":
+        Script101();
+        break;
+      case "6AgRJQ2Q4AF":
+        Script102();
+        break;
+      case "5wmluKcAjtf":
+        Script103();
+        break;
+      case "62UlqCg7p5G":
+        Script104();
+        break;
+      case "6bgTOEfNmfk":
+        Script105();
+        break;
+      case "6Hgz6RLVKLl":
+        Script106();
+        break;
+      case "65gtQfTj6uR":
+        Script107();
+        break;
+      case "6Nt4fOsRkov":
+        Script108();
+        break;
+      case "6HjrfK2gw4d":
+        Script109();
+        break;
+      case "5kudbxrz9fI":
+        Script110();
+        break;
+      case "5W1BeOpnuUf":
+        Script111();
+        break;
+      case "5bKzPGtI8PN":
+        Script112();
+        break;
+      case "5euiMbBV0Mw":
+        Script113();
+        break;
+      case "6kxKwSgway9":
+        Script114();
+        break;
+      case "6I72bgbMRMD":
+        Script115();
+        break;
+      case "5ouutLtnTgD":
+        Script116();
+        break;
+      case "5eYPmxL3sFt":
+        Script117();
+        break;
+      case "5qTwVUIN8Lo":
+        Script118();
+        break;
+      case "5YMfZ9S7tZa":
+        Script119();
+        break;
+      case "6dQEONn2xI7":
+        Script120();
+        break;
+      case "5hQ21iHRbbo":
+        Script121();
+        break;
+      case "6lnWTMqRoCv":
+        Script122();
+        break;
+      case "6Nxe8gVdDLP":
+        Script123();
+        break;
+  }
+}
+
+function Script1()
+{
+  if (!window.bgMusic) {
+    window.bgMusic = new Audio("backsound.mp3");
+
+    // Perintah loop standar
+    window.bgMusic.loop = true; 
+
+    // Perintah paksaan (pengaman) jika lagunya habis
+    window.bgMusic.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+
+    window.bgMusic.play();
+}
+}
+
+function Script2()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script3()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script4()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script5()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script6()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script7()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script8()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script9()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script10()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script11()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script12()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script13()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script14()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script15()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script16()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script17()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script18()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script19()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script20()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script21()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script22()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script23()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script24()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script25()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script26()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script27()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script28()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script29()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script30()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script31()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script32()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script33()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script34()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script35()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script36()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script37()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script38()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script39()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script40()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script41()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script42()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script43()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script44()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script45()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script46()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script47()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script48()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script49()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script50()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script51()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script52()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script53()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script54()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script55()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script56()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script57()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script58()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script59()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script60()
+{
+  var iframe = document.getElementsByTagName('iframe')[0];
+if (iframe) {
+    var iframeDoc = iframe.contentWindow.document;
+    var audio = iframeDoc.getElementsByTagName('audio')[0];
+    if (audio) {
+        audio.play(); 
+    }
+}
+}
+
+function Script61()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script62()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script63()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script64()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script65()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script66()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script67()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script68()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script69()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script70()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script71()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script72()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script73()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script74()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script75()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script76()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script77()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script78()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script79()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script80()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script81()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script82()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script83()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script84()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script85()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script86()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script87()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script88()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script89()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script90()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script91()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script92()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script93()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script94()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script95()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script96()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script97()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script98()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script99()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script100()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script101()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script102()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script103()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script104()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script105()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script106()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script107()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script108()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script109()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script110()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script111()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script112()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script113()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script114()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script115()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script116()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script117()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script118()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script119()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script120()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script121()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
+function Script122()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script123()
+{
+  if (window.bgMusic) {
+    window.bgMusic.play();
+}
+}
+
